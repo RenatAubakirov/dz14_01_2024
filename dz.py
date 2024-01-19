4709de2c9c65c429ecfa00db1aad7f16024ca4c6
Renat Aubakirov <arenat98@mail.ru>
2024-01-19 19:44:42 +0700

    dz.py

--- dz.py
+++ dz.py
@@ -8,3 +8,21 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
 """
+
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+data.head()
+
+def one_hot(data, column_name):
+    unique_values = data[column_name].unique()
+    for value in unique_values:
+       data[f'whoAmI_{value}'] = (data[column_name] == value).astype(int)
+
+    data.drop(column_name, axis=1, inplace=True)
+
+one_hot(data, 'whoAmI')
+print(data)
